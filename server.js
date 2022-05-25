2b7a56649546ae436bdc3a7cfe854f6f88d144ff
Sridhar <swarup@nuevesolutions.com>
2022-05-25 15:19:53 +0530

    make it better

--- server.js
+++ server.js
@@ -29,26 +29,26 @@ mongoose
 
 
 
-// if (process.env.NODE_ENV === 'production') {
-//     app.use(express.static('client/build'))
-//     app.get('*', (req, res) => {
-//         res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
-//     })
-// }
-
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/build')));
-
-    app.get('*', (req, res) =>
-        res.sendFile(
-            path.resolve(__dirname, '..', 'client', 'build', 'index.html')
-        )
-    );
-} else {
-    app.get('/', (req, res) => {
-        res.send('API is running....');
-    });
+    app.use(express.static('client/build'))
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+    })
 }
+
+// if (process.env.NODE_ENV === 'production') {
+//     app.use(express.static(path.join(__dirname, '../client/build')));
+
+//     app.get('*', (req, res) =>
+//         res.sendFile(
+//             path.resolve(__dirname, '..', 'client', 'build', 'index.html')
+//         )
+//     );
+// } else {
+//     app.get('/', (req, res) => {
+//         res.send('API is running....');
+//     });
+// }
 
 
 

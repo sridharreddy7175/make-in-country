5053287a9601909fb934c26afbd4f253666aa9c7
Sridhar <swarup@nuevesolutions.com>
2022-05-26 15:46:21 +0530

    make it better

--- server.js
+++ server.js
@@ -30,20 +30,15 @@ mongoose
 
 
 // Home page url for application
-// if (process.env.NODE_ENV === "production") {
-//     app.use(express.static(path.join(__dirname, 'client', 'build')));
-//     app.get('/', (request, response) => {
-//         response.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
-//     });
-// }
-
-
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'))
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
-    })
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, 'client', 'build')));
+    app.get('/', (request, response) => {
+        response.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+    });
 }
+
+
+
 
 
 
